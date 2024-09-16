be80f45c7431363154ad13139d356b1bcb29a931
somdipdatta <somdip@alumni.princton.edu>
2024-09-16 11:06:32 -0400

    refactor duration

--- src/price.py
+++ src/price.py
@@ -3,6 +3,19 @@ import numpy as np
 from qablet.base.fixed import FixedModel
 from qablet_contracts.timetable import py_to_ts
 from src.bond import bond_dict_to_obj
+import copy
+
+
+def price_shocked(model, timetable, dataset_orig, shock):
+    dataset = copy.deepcopy(dataset_orig)
+    zero_rates = dataset["ASSETS"]["USD"][1]
+    zero_rates[:, 1] = zero_rates[:, 1] + shock
+    dataset["ASSETS"]["USD"] = ("ZERO_RATES", zero_rates)
+
+    price, _ = model.price(timetable, dataset)
+
+    return price
+
 
 def update_price(data, rate_data):
     """Update missing prices and calculate duration for all bonds in the table, in place."""
@@ -21,7 +34,7 @@ def update_price(data, rate_data):
         "PRICING_TS": py_to_ts(datetime(2023, 12, 31)).value,
         "ASSETS": {"USD": discount_data},
     }
-    
+
     model = FixedModel()
     shock_size = 0.01  # 1% rate shock
 
@@ -38,34 +51,9 @@ def update_price(data, rate_data):
         price, _ = model.price(timetable, dataset)
         bond["Price"] = f"${price * notional:.6f}"
 
-        # 2. Shock rates up by 1%
-        discount_data_up = (
-            "ZERO_RATES",
-            np.array([[rate["Year"], (rate["Rate"] / 100) + shock_size] for rate in rate_data]),
-        )
-        dataset_up = {
-            "BASE": "USD",
-            "PRICING_TS": py_to_ts(datetime(2023, 12, 31)).value,
-            "ASSETS": {"USD": discount_data_up},
-        }
-        price_up, _ = model.price(timetable, dataset_up)
-
-        # 3. Shock rates down by 1%
-        discount_data_down = (
-            "ZERO_RATES",
-            np.array([[rate["Year"], (rate["Rate"] / 100) - shock_size] for rate in rate_data]),
-        )
-        dataset_down = {
-            "BASE": "USD",
-            "PRICING_TS": py_to_ts(datetime(2023, 12, 31)).value,
-            "ASSETS": {"USD": discount_data_down},
-        }
-        price_down, _ = model.price(timetable, dataset_down)
-
-        # 4. Calculate DV (Dollar Value of 1%)
+        price_up = price_shocked(model, timetable, dataset, shock=shock_size)
+        price_down = price_shocked(model, timetable, dataset, shock=-shock_size)
         dv = (price_up - price_down) / (2 * shock_size)
 
         # 5. Calculate duration
-        duration = abs(dv / price)
-        bond["Duration"] = f"{duration:.6f}"  # Add duration to the bond data
-        
+        bond["Duration"] = f"{-dv / price:.6f}"  # Add duration to the bond data
